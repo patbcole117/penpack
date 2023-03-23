55ebaf8337e0ab664eddad27ac641ff11386db9b
Patrick Coleman <pcolemanofficial@gmail.com>
2023-03-23 18:57:23 +0530

    Now supports encoding chains. BBB will triple b64 encode. BUB will b64 - url - b64 etc.

--- tools/mytools/endycoder.py
+++ tools/mytools/endycoder.py
@@ -7,53 +7,64 @@ def main():
 
     ap = ArgumentParser()
     ap.add_argument('instring')
-    ap.add_argument('-t', '--type')
+    ap.add_argument('-t', '--typechain')
     ap.add_argument('-d', '--decode', action="store_true")
     ap.add_argument('-n','--nospace',action="store_true")
     ap.add_argument('-f','--file',action="store_true")
     args = ap.parse_args()
 
-    print(f"DEBUG: {args}")
+    print(f"DEBUG: ARGS: {args}")
 
     if args.file:
         f = open(args.instring, 'r')
         instring = f.read()
     else:
         instring = args.instring
+    
+    for t in args.typechain:
+        if t.upper() == 'U':
+            if args.decode:
+               instring = decode_url(instring)
+            else:
+                instring = encode_url(instring, args.nospace)
+        elif t.upper() == 'B':
+            if args.decode:
+               instring = decode_b64(instring)
+            else:
+                instring = encode_b64(instring)
 
-    if args.type.upper() == 'URL':
-        print(url_format(instring, args.decode, args.nospace))
-    elif args.type.upper() == 'B64':
-        print(b64_format(instring, args.decode))
+    print(instring)
 
-def url_format(instring, d, n):
+def encode_url(instring, nospace):
 
-    if d:
-        print('DECODING URL....')
-        outstring = urllib.parse.unquote(instring)
-        return outstring
-    else:
-        print('ENCODING URL....')
-        outstring = urllib.parse.quote(instring, safe='')
-        outstring = outstring.replace('.', '%2E')
-        outstring = outstring.replace('-','%2D')
-        if n:outstring = outstring.replace('%20', '%09')
-        return outstring
+    print('ENCODING URL....')
+    outstring = urllib.parse.quote(instring, safe='')
+    outstring = outstring.replace('.', '%2E')
+    outstring = outstring.replace('-','%2D')
+    if nospace:outstring = outstring.replace('%20', '%09')
+    return outstring
 
-def b64_format(instring, d):
+def decode_url(instring):
+     
+    print('DECODING URL....')
+    outstring = urllib.parse.unquote(instring)
+    return outstring
 
+def encode_b64(instring):
+
+    print('ENCODING B64....')
     instring_bytes = instring.encode('ascii')
+    outstring_bytes = base64.b64encode(instring_bytes)
+    outstring = outstring_bytes.decode('ascii')
+    return outstring
 
-    if d:
-        print('DECODING B64....')
-        outstring_bytes = base64.b64decode(instring_bytes)
-        outstring = outstring_bytes.decode('ascii')
-        return outstring
-    else:
-        print('ENCODING B64....')
-        outstring_bytes = base64.b64encode(instring_bytes)
-        outstring = outstring_bytes.decode('ascii')
-        return outstring
+def decode_b64(instring):
+
+    print('DECODING B64....')
+    instring_bytes = instring.encode('ascii')
+    outstring_bytes = base64.b64decode(instring_bytes)
+    outstring = outstring_bytes.decode('ascii')
+    return outstring
 
 if __name__ == '__main__':
     main()
