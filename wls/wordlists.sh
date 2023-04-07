2476c6e4bf4554f56a14095d7dc496ee508abe67
Patrick Coleman <pcolemanofficial@gmail.com>
2023-04-08 04:12:59 +0530

    wordlist rework

---
+++ wls/wordlists.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-medium-directories-lowercase.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-medium-directories.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top1million-5000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/bitquark-subdomains-top100000.txt
