f8a44d94b264df292e1fc7bfde9bf9dbc959c44b
Patrick Coleman <pcolemanofficial@gmail.com>
2023-04-08 13:22:40 +0530

    added som handy firsttimesetup notes.

---
+++ configs/things.sh
@@ -0,0 +1,18 @@
+# /usr/share/first-time-setup/first-time-setup.sh
+
+ftsdir='/usr/share/first-time-setup/'
+
+# ZSH
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# ZSH SYNTAX HILIGHTING
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ftsdir}zsh-syntax-hilighting
+echo "source ${ftsdir}zsh-syntax-hilighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source /usr/share/first-time-setup/zsh-syntax-hilighting/zsh-syntax-highlighting.zsh
+
+# TMUX PLUGINS
+https://github.com/tmux-plugins/tpm
+
+# TMUX THEMES
+https://github.com/jimeh/tmux-themepack
