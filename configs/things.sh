330d195db07239cc0265042d4c3de5b51519f531
Patrick Coleman <pcolemanofficial@gmail.com>
2023-04-21 17:50:34 +0530

    restructuring

--- configs/things.sh
+++ configs/things.sh
@@ -1,16 +1,3 @@
-# /usr/share/first-time-setup/first-time-setup.sh
-
-ftsdir='/usr/share/first-time-setup/'
-
-# ZSH
-sudo apt install zsh
-chsh -s $(which zsh)
-
-# ZSH SYNTAX HILIGHTING
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ftsdir}zsh-syntax-hilighting
-echo "source ${ftsdir}zsh-syntax-hilighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source /usr/share/first-time-setup/zsh-syntax-hilighting/zsh-syntax-highlighting.zsh
-
 # TMUX
 https://github.com/tmux-plugins/tpm
 https://github.com/tmux-plugins/tmux-resurrect
