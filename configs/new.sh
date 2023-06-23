9535a6516c023060de745ddfe75c8793fd6d8c04
patbcole117 <75111838+patbcole117@users.noreply.github.com>
2023-06-24 03:24:46 +0530

    Update new.sh

--- configs/new.sh
+++ configs/new.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://raw.githubusercontent.com/patbcole117/penpack/master/configs/.tmux.conf .tmux.conf
 wget https://raw.githubusercontent.com/patbcole117/penpack/master/configs/.vimrc .vimrc
+sudo apt install vim-gui-common
+sudo apt install vim-runtime
